5f9de38fb4bc2e6ee62cd2d51b9e83595c0f0d2b
boB Rudis <bob@rud.is>
2018-09-29 09:52:34 -0400

    R package repo initialization complete

---
+++ R/unsound-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/unsound>
+#' - BugReports: <https://gitlab.com/hrbrmstr/unsound/issues>
+#' 
+#' @md
+#' @name unsound
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
